8f340bfdf4873aa3f192c2f17de5a44964fcbd57
vonbarown <vonielbrown@pursuit.org>
2019-11-23 20:26:49 -0500

    fixing name tag

--- src/components/userInfo.jsx
+++ src/components/userInfo.jsx
@@ -13,7 +13,7 @@ class UserInfo extends React.Component {
       <div className="profileInfo">
         <img src={this.user.imageLink} alt="hello" />
         <div className="info">
-          <p>{this.user.userName}</p>
+          <h3>{this.user.userName}</h3>
           <h4>{this.user.title}</h4>
           <p>{this.user.description}</p>
         </div>
