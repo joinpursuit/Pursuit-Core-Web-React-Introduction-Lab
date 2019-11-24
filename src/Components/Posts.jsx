c041bc73e2e7e8bd4e809c2995e012297219e97d
Owen Jones <owenthefirst93@gmail.com>
2019-11-23 19:09:44 -0500

    borders

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 class Posts extends React.Component {
     render() {
         return (
-            <div>
+            <div className= 'container'>
                 <h4>Posts</h4>
-                <div>
+                <div className= 'post'>
                     <h4>Title</h4>
                     <p>Lorem Ipsum</p>
                 </div>
-                <div>
+                <div className= 'post'>
                     <h4>Title</h4>
                     <p>Lorem Ipsum</p>
                 </div>
-                <div>
+                <div className= 'post'>
                     <h4>Title</h4>
                     <p>Lorem Ipsum</p>
                 </div>
