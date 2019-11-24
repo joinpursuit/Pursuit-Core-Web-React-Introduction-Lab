be9dcc41ee57359951e5e7d4842327c73935385c
sergiocohens <sergiocohensalama@pursuit.org>
2019-11-24 18:11:03 -0500

    lab

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar.js'
+import UserProfile from './Components/UserProfile.js'
+import Posts from './Components/Posts.js'
+import Contacts from './Components/Contacts.js'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {NavBar()}
+    {UserProfile()}
+    {Posts()}
+    {Contacts()}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/Posts.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Posts = () => {
+  return (
+    <div className='Posts'>
+    <h1> Posts </h1>
+    <div className='PostCard'>
+    <h1 className='PostTitle'> Title </h1>
+    <p className='PostBody'> Lorem Ipsem</p>
+    </div>
+    <div className='PostCard'>
+    <h1 className='PostTitle'> Title </h1>
+    <p className='PostBody'> Lorem Ipsem</p>
+    </div>
+    <div className='PostCard'>
+    <h1 className='PostTitle'> Title </h1>
+    <p className='PostBody'> Lorem Ipsem</p>
+    </div>
+    </div>
+  )
+}
+
+export default Posts
