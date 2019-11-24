07aa9d2ca3ad5ceb721cdf861442c34e945f6d8c
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-11-24 14:17:59 -0500

    created utils, moved helpers to utils

---
+++ src/utils/helpers.js
@@ -0,0 +1,56 @@
+/*
+Joseph P. Pasaoa
+Helpers | React Introduction Lab
+*/
+
+
+/* FUNCTIONS */
+const log = console.log;
+
+
+/* DATA SIM */
+let contactNum = -1;
+const data = {
+  user: {
+    name: "Jung Rae Jang",
+    position: "React Assistant",
+    network: "Lorem Ipsem",
+    avatar_url: require('../FauxDb/avatar-ph-user.png')
+  },
+  contacts: [
+    {
+      name: "Alejandro Franco",
+      position: "React Expert",
+      network: "Lorem Ipsem",
+      avatar_url: require('../FauxDb/avatar-ph-1.png')
+    },
+    {
+      name: "Dessa Shepherd",
+      position: "React Manager",
+      network: "Lorem Ipsem",
+      avatar_url: require('../FauxDb/avatar-ph-2.png')
+    },
+    {
+      name: "Wynter Reid",
+      position: "React Assistant",
+      network: "Lorem Ipsem",
+      avatar_url: require('../FauxDb/avatar-ph-3.png')
+    },
+    {
+      name: "David Yang",
+      position: "Co-Founder",
+      network: "Lorem Ipsem",
+      avatar_url: require('../FauxDb/avatar-ph-4.png')
+    },
+    {
+      name: "Jukay Hsu",
+      position: "Co-Founder",
+      network: "Lorem Ipsem",
+      avatar_url: require('../FauxDb/avatar-ph-4.png')
+    }
+  ]
+}
+
+
+/* EXPORTS */
+export { log, contactNum, data };
