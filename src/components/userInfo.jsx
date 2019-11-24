c9ff784afcffe57d2f3cdc7942092d0c2834f37a
vonbarown <vonielbrown@pursuit.org>
2019-11-23 20:25:50 -0500

    finishing grid layout

--- src/components/userInfo.jsx
+++ src/components/userInfo.jsx
@@ -5,6 +5,7 @@ class UserInfo extends React.Component {
     userName: "Jordan Walke",
     imageLink:
       "https://www.stockvault.net/data/2007/03/01/100169/preview16.jpg",
+    title: "React Creator",
     description: "Lorem Ipsem"
   };
   render() {
@@ -13,6 +14,7 @@ class UserInfo extends React.Component {
         <img src={this.user.imageLink} alt="hello" />
         <div className="info">
           <p>{this.user.userName}</p>
+          <h4>{this.user.title}</h4>
           <p>{this.user.description}</p>
         </div>
       </div>
