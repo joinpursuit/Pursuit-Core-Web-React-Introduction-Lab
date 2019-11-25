6ad23387d6ec3d88f02bed6568ea354fe3391981
Bpmauge <briahanamauge@pursuit.org>
2019-11-25 18:43:39 -0500

    styled nav bar

--- src/Components/navBar.jsx
+++ src/Components/navBar.jsx
@@ -4,12 +4,11 @@ import './navBar.css';
 class NavBar extends React.Component{
     render() {
         return (
-            <div className = 'navbar'>
+            <nav>
                 <button>What is Pursuit</button>
                 <button>Create an account</button>
                 <button>Sign up</button>
-
-            </div>
+            </nav>
         )
     }
 }
