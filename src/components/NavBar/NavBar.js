16e7f398faca7f7f00bbed652aa8593ece007f9f
Henrynunez112 <henrynunez@pursuit.org>
2020-02-15 12:10:31 -0500

    just created NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class NavBar extends React.Component{
+    render(){
+        return(
+            <div>
+                <button>What is Pursuit?</button>
+                <button>Create an account</button>
+                <button>Sign In</button>
+                
+            </div>
+        )
+    }
+}
+
+export default NavBar;
