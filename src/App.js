1a1ce464b61f6ec473bfdddd8fe3985789e75854
david maravillas <davidmaravillas@pursuit.org>
2020-02-15 12:59:39 -0500

    test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <NavBar links = {[{urlName:"What is Pursuit",urlLink:"2"},{urlName:"Create an account",urlLink:"2"},{urlName:"Sign in",urlLink:"2"}]}/>
       
-      <PostList post = {[{title:"1",content:"1"},{title:"2",content:"2"},{title:"3",content:"4"}]}/>
+      <PostList post = {[{post_id:"1", title:"Title 1",content:"content is here"},{post_id:"2", title:"Title 2",content:"content2 is here"},{post_id:"3", title:"Title 3",content:"content3 is here"},]}/>
       <ContactList contacts={[{name:"jim",position:"test",statement:"test2"}]}/>
     </div>
 
