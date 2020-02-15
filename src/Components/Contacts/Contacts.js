a1ecc87befb7a7efe5c84570715025caed57b26d
Ohidurr <Ohidurrahman96@gmail.com>
2020-02-15 12:56:50 -0500

    add contacts styling

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -3,10 +3,21 @@ import './contacts.css';
 
 const Contacts = (props) => {
     return (
-        <div className={"person"}>
-            Name: {props.name},
-            Title: {props.title},
-            Summary: {props.summary}
+        <div className={"Person"}>
+            <div className="image">
+                <img src ="https://i.pinimg.com/236x/e1/a7/b6/e1a7b6048ebf26655cc593a2b415b812.jpg"></img>
+            </div>
+            <div className="list">
+                <div id="name">
+                    {props.name}
+                </div>
+                <div id="title">
+                    {props.title}
+                </div>
+                <div id="summary">
+                    {props.summary}
+                </div>
+            </div>
         </div>
     )
 }
