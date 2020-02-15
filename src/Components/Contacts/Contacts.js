498e0c0d309f29ffdf86df53f75dd8d8367d2792
Ohidurr <Ohidurrahman96@gmail.com>
2020-02-15 15:53:52 -0500

    add contact styling and create post.css

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -5,7 +5,7 @@ const Contacts = (props) => {
     return (
         <div className={"Person"}>
             <div className="image">
-                <img src ="https://i.pinimg.com/236x/e1/a7/b6/e1a7b6048ebf26655cc593a2b415b812.jpg"></img>
+                <img className ="picture" src ="https://i.pinimg.com/236x/e1/a7/b6/e1a7b6048ebf26655cc593a2b415b812.jpg"></img>
             </div>
             <div className="list">
                 <div id="name">
--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './post.css'
 const Posts = (props) => {
     return (
         <div className={"onePost"}>
