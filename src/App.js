92e6e7b4391cd6de5896e6ac2916ee59aae6dbb7
maria liza <mariamartinez@pursuit.org>
2020-02-15 15:47:24 -0500

    continue styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import NavBar from './components/NavBar';
-import UserProfile from './components/UserProfile';
-import Posts from './components/Posts';
-import Contacts from './components/Contacts';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import UserProfile from "./components/UserProfile";
+import Posts from "./components/Posts";
+import Contacts from "./components/Contacts";
 // import ContactUserCard from './components/ContactUserCard';
 
 function App() {
   return (
     <div className="App">
-      <Contacts contactList= {[{name: "Sam", title: "Student", caption: "Hey", image: "img1"}, {name: "Maria", title: "Student", caption: "Bonjour", image: "img2"}]} />
-      <Posts posts= {[{title: "Cool", caption: "Hey"}]} />
+      <Contacts
+        contactList={[
+          { name: "Sam", title: "Student", caption: "Hey", image: "img1" },
+          { name: "Maria", title: "Student", caption: "Bonjour", image: "img2" }
+        ]}
+      />
+      <Posts posts={[{ title: "Cool", caption: "Hey" }]} />
       {/* <UserProfile [name: "Maria", title: "Student", caption: "Bonjour"] /> */}
       <NavBar />
     </div>
