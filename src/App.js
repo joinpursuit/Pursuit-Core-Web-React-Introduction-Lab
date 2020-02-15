8b18b1a117b07d0093e3fae72983c830b8a3ba06
david maravillas <davidmaravillas@pursuit.org>
2020-02-15 13:46:43 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import NavBar from "./components/navBar/navBar"
-import PostList from './components/postList/postList'
-import ContactList from "./components/ContactList/contactList"
+import NavBar from "./components/navBar/navBar";
+import PostList from './components/postList/postList';
+import ContactList from "./components/ContactList/contactList";
+import Profile from "./components/userProfile.js"
 import './App.css';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       // console.log({url,urlName})  
     <div className="App">
       <NavBar links = {[{urlName:"What is Pursuit",urlLink:"2"},{urlName:"Create an account",urlLink:"2"},{urlName:"Sign in",urlLink:"2"}]}/>
-      
+      <Profile name={"Jordan Walke"} position={"react Creator"} statement={"lorem ipsom"}/>
       <PostList post = {[{post_id:"1", title:"Title 1",content:"content is here"},{post_id:"2", title:"Title 2",content:"content2 is here"},{post_id:"3", title:"Title 3",content:"content3 is here"},]}/>
 
       <ContactList contacts={[{name:"jim",position:"test",statement:"test2"}]}/>
---
+++ src/components/userProfile.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Profile = ({name,position,statement})=>{
+    return(
+        <div className={"profile"}>
+            <img src="https://image.shutterstock.com/image-vector/male-silhouette-avatar-profile-picture-260nw-199246382.jpg" alt="profile"/>
+            <div>
+                <h3>{name}</h3>
+                <h4>{position}</h4>
+                <h5>{statement}</h5>
+            </div>
+
+        </div>
+    )
+}
+
+
+export default Profile;
