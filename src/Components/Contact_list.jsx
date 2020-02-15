b6390592b26d47ea21531d9e1af94ad2b0e92251
bsihame <sihamebazi@pursuit.org>
2020-02-15 12:18:57 -0500

    add class to contact list and posts

--- src/Components/Contact_list.jsx
+++ src/Components/Contact_list.jsx
@@ -1,4 +1,27 @@
 import React from 'react'
 import Contacts from './Contacts'
 import './Contact_list.css'
+ class Contact_list extends React.Component {
+   constructor() {
+     super ();
+     this.info ={
+       name: 'Marck',
+       image: Avatar,
+       title: 'Creator',
+       description:'Hello word',
+   }
+ }
+
+ render(){
+   return(
+     <div>
+       <img src={this.info.image} alt="picture of a person"></img>
+       <h3> {this.info.name}</h3>
+       <h4>{this.info.title}</h4>
+       <p>{this.info.description}</p>
+     </div>
+   )
+ }
+ }
+
 export default Contact_list;
--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -1,4 +1,24 @@
 import React from 'react';
 import UserProfile from './User_profile'
 import './Posts.css'
+
+class Post extends React.Component {
+  constructor() {
+    super();
+    this.info ={
+      title: 'Title',
+      description: 'Description'
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <h3>{this.info.title}</h3>
+        <p>{this.info.description}</p>
+      </div>
+    )
+  }
+}
+
 export default Posts;
