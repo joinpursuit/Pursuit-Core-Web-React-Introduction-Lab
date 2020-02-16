e0f9efa0203b94980acccc660e45c8bba4ff1d78
bsihame <sihamebazi@pursuit.org>
2020-02-16 07:43:49 -0500

    add css to contact list

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 
-// import './Contacts.css'
+ import './Contact.css'
 import ContactList from './ContactList';
+
 
 class Contact extends React.Component {
     render(){
         return(
             <div>
+                <div id="contacts">
                 <h2>Contacts</h2>
                 <ContactList/>
                 <ContactList/>
                 <ContactList/>
                 <ContactList/>
+                </div>
+                <div>
+                </div>
+
+                
             </div>
         )
     }
--- src/Components/ContactList.jsx
+++ src/Components/ContactList.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import'./ContactList.css'
+const avatar = require ('./Avatars/Avatar.png')
 
  class ContactList extends React.Component {
    constructor() {
      super ();
      this.info ={
        name: 'Mark',
-
        title: 'Creator',
        description:'Hello word',
    }
@@ -13,11 +14,17 @@ import React from 'react'
 
  render(){
    return(
-     <div>
-  
+     <div id='contact'>
+       <div class="user">
+       <div class='image'>
+      <img src={avatar} alt="avatar"></img>
+       </div>
+       <div class='userInfo'>
        <h3> {this.info.name}</h3>
        <h4>{this.info.title}</h4>
        <p>{this.info.description}</p>
+       </div>
+       </div>
      </div>
    )
  }
