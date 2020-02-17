b2052f5041d2628033157a64d034394b0bc148b8
Kevin Wong <kevinwong@pursuit.org>
2020-02-17 00:55:09 -0500

    add component files and configure app

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="NavBar">
+
+      </div>
+
+      <div className="Contacts">
+
+      </div>
+
+      <div className="Posts">
+
+      </div>
+
+      <div className="UserProfile">
+
+      </div>
     </div>
   );
 }
