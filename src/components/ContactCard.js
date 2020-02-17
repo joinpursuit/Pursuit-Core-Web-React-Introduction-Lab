c2950f891ce1b537d881a1be3c484e4c7e7a586f
Kevin <kevinwong@pursuit.org>
2020-02-17 02:57:05 -0500

    start Contacts and ContactCard components

---
+++ src/components/ContactCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ContactCard extends React.Component {
+  render() {
+    return (
+      <div className="contactCard">
+        <h3>Jordan Walker</h3>
+        <p>React Developer</p>
+        <p>Lorem Ipsem</p>
+      </div>
+    );
+  }
+}
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "../master/Contacts.css";
+import ContactCard from "../components/ContactCard.js";
 
 class Contacts extends React.Component {
   render() {
-    return <div className="Contacts"></div>;
+    return (
+      <div className="Contacts">
+        <h3>Contacts</h3>
+        <ContactCard />
+        <ContactCard />
+        <ContactCard />
+      </div>
+    );
   }
 }
 
