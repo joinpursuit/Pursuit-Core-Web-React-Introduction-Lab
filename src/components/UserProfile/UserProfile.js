97cd52221aa3493af32731facd38c58fa9e26212
Phillip <phil.awich@gmail.com>
2020-02-18 17:22:28 -0500

    Test UserProfile.js

---
+++ src/components/UserProfile/UserProfile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './UserProfile.css';
+
+const userProfile = (props) =>(
+    <div className="userProfile">
+        <p>This is User Profile</p>
+    </div>
+)
+
+export default userProfile;
