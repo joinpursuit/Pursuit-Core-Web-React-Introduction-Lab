e227154a5aae069db44cba841bee5aeb0bb8360e
Phillip <phil.awich@gmail.com>
2020-02-18 17:50:04 -0500

    Complete UserProfile

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -4,7 +4,11 @@ import './UserProfile.css';
 const userProfile = (props) =>(
     <div className="userProfile">
         <div className="userProfile-picture">
-
+            <div className="userProfile-summary">
+                <h1>Kwon Ji-yong</h1>
+                <h5>React Creator</h5>
+                <p>This is a sample bio of React developer</p>
+            </div>
         </div>
     </div>
 )
