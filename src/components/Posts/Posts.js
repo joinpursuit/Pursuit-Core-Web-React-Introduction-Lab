9dbdc9505b5af824ed6e44d46c8e25777b5d7bcb
Phillip <phil.awich@gmail.com>
2020-02-18 18:25:02 -0500

    Complete Posts component

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -3,6 +3,15 @@ import './Posts.css';
 
 const posts = (props) => (
     <div className="posts">
+        <div className="posts-first">
+            <h1>Insert Here</h1>
+        </div>
+        <div className="posts-second">
+            <h1>Insert Here</h1>
+        </div>
+        <div className="posts-third">
+            <h1>Insert Here</h1>
+        </div>
     </div>
 )
 
