0b9a3ef3fb0f24bb6bebeae463700db6bbe1ef06
Phillip <phil.awich@gmail.com>
2020-02-18 13:17:00 -0500

    Create Navbar with logo and nav items

---
+++ src/components/Toolbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Navbar.css'
+
+const navBar = (prop) => (
+    <header className="toolbar">
+        <nav className="toolbar_navigation">
+            <div></div>
+            <div className="toolbar_logo"><a href="/">Pursuit</a></div>
+            <div className="spacing-between"></div>
+            <div className="toolbar_navigation-items">
+                <ul>
+                    <li><a href='/'>What is Pursuit</a></li>
+                    <li><a href='/'>Create an account</a></li>
+                    <li><a href='/'>Sign In</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+)
+
+export default navBar;
