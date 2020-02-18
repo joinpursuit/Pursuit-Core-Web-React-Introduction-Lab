e246efd4051592a795e4075327259ae7a1ee3feb
Phillip <phil.awich@gmail.com>
2020-02-18 18:51:25 -0500

    Add Contacts

---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Contacts.css'
+
+const contacts = (props) => (
+    <div className="contacts">
+        <h1>Contacts</h1>
+    </div>
+)
+
+export default contacts;
