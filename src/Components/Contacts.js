81ca09e4767aa165e0ffc57ef842439ef82833d1
Kelvin <kelvinarellano@pursuit.org>
2020-02-17 20:18:00 -0500

    lab done

---
+++ src/Components/Contacts.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Contacts = () => {
+  return (
+    <div className='Contacts'>
+    <h1>Contacts</h1>
+    <div className='ContactCard'>
+    <img className='ContactPic' src='https://secondchancetinyhomes.org/wp-content/uploads/2016/09/empty-profile.png'></img>
+    <div className='ContactInfo'>
+    <h1 className='ContactName'>Jordan Walker</h1>
+    <h2 className='ContactRole'>React Creator</h2>
+    <p className='ContactBody'>Loren Ipsem</p>
+    </div>
+    </div>
+    <div className='ContactCard'>
+    <img className='ContactPic' src='https://secondchancetinyhomes.org/wp-content/uploads/2016/09/empty-profile.png'></img>
+    <div className='ContactInfo'>
+    <h1 className='ContactName'>Jordan Walker</h1>
+    <h2 className='ContactRole'>React Creator</h2>
+    <p className='ContactBody'>Loren Ipsem</p>
+    </div>
+    </div>
+    <div className='ContactCard'>
+    <img className='ContactPic' src='https://secondchancetinyhomes.org/wp-content/uploads/2016/09/empty-profile.png'></img>
+    <div className='ContactInfo'>
+    <h1 className='ContactName'>Jordan Walker</h1>
+    <h2 className='ContactRole'>React Creator</h2>
+    <p className='ContactBody'>Loren Ipsem</p>
+    </div>
+    </div>
+    <div className='ContactCard'>
+    <img className='ContactPic' src='https://secondchancetinyhomes.org/wp-content/uploads/2016/09/empty-profile.png'></img>
+    <div className='ContactInfo'>
+    <h1 className='ContactName'>Jordan Walker</h1>
+    <h2 className='ContactRole'>React Creator</h2>
+    <p className='ContactBody'>Loren Ipsem</p>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Contacts
---
+++ src/Components/UserProfile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const UserProfile = () => {
+  return (
+    <div className='UserProfile'>
+    <img className='UserPic' src='https://secondchancetinyhomes.org/wp-content/uploads/2016/09/empty-profile.png'></img>
+    <div className='UserInfo'>
+    <h1 className='UserName'>Jordan Walker</h1>
+    <h2 className='UserRole'>React Creator</h2>
+    <p className='UserBody'>Loren Ipsem</p>
+    </div>
+    </div>
+  )
+}
+
+export default UserProfile
