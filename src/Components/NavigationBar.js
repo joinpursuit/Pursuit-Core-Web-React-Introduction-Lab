32548ffdaf6e6ffbd7ceb8bfbd8ba7917802afaa
Nilber Remon <nilberremon@gmail.com>
2020-02-21 22:56:41 -0500

    NavBar and UserProfile

---
+++ src/Components/NavigationBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../CSS/NavigationBar.css';
+
+const NavigationBar = () => {
+    return (
+        <div>
+            <button>What is Pursuit?</button>
+            <button>Create User</button>
+            <button>Sign In</button>
+        </div>
+    )
+}
+
+export default NavigationBar;
--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,3 +1,19 @@
+import React from 'react'
+import '../CSS/UserProfile.css'
 
+const UserProfile = (user) => {
+    return (
+        <div className={"userProfileContainer"}>
+            <div className="imageContainer">
+                <img className="picture" src ="https://img.icons8.com/cotton/2x/gender-neutral-user--v1.png"></img>
+            </div>
+            <div className="profileInfo">
+                <div id="name"> {user.name} </div>
+                <div id="jobtTtle"> {user.jobTitle} </div>
+                <div id="bio"> {user.bio} </div>
+            </div>
+        </div>
+    )
+}
 
 export default UserProfile
