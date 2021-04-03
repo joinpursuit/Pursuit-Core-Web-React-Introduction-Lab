d44aee1edbc637792020d256cae94986bb9adc82
Daliz Cruz <dalizcruz@gmail.com>
2021-04-03 17:13:09 -0400

    lab done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import NavBar from "./Components/NavBar"
+import UserProfile from "./Components/UserProfile"
+import Posts from "./Components/Posts"
+import Contacts from "./Components/Contacts"
+// import ContactUserCard from "./Components/ContactUserCard"
 import "./index.css";
 
 export default function App() {
-  return <p>Hello, world!</p>;
+  return (
+    <>
+    <NavBar />
+    <UserProfile />
+    <Posts />
+    <Contacts />
+    </>
+  );
 }
+
---
+++ src/Components/Contacts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ContactUserCard from "./ContactUserCard"
+
+const Contacts = () => {
+    return (
+        <>
+        <ContactUserCard />
+        <ContactUserCard />
+        <ContactUserCard />
+        <ContactUserCard />
+        </>
+    )
+};
+
+export default Contacts;
