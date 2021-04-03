6465ca4cce73ae5c9009234269e0ffeab72fa64f
Teraesa  Metivier <teraesam@yahoo.com>
2021-04-03 16:21:44 -0400

    started user profilr

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,19 +1,13 @@
 import React from "react";
 
 const NavBar = () => {
-    return (
-        <header className="NavBar">
-            <button>
-                Sign in
-            </button>
-            <button>
-                Create an account
-            </button>
-            <button>
-                What is Pursuit?
-            </button>
-        </header>
-    )
-}
+  return (
+    <header className="Nav-Bar">
+      <button>Sign in</button>
+      <button>Create an account</button>
+      <button>What is Pursuit?</button>
+    </header>
+  );
+};
 
 export default NavBar;
--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,10 +1,22 @@
 import React from "react";
 
+const userImage = {
+  imgLink:
+    "/Users/teraesametivier/Desktop/REACT/Pursuit-Core-Web-React-Introduction-Lab/public/profile.png",
+};
+
 const UserProfile = () => {
   return (
-    <>
-      <p> hello</p>
-    </>
+    <section className="user-profile">
+      <div id="image">
+        <img src={userImage.imgLink} width="100" height="100"></img>
+      </div>
+      <div id="content">
+        <h4>Jordan Walke</h4>
+        <h6>React Creator</h6>
+        <p> Lorem Ipsum</p>
+      </div>
+    </section>
   );
 };
 
