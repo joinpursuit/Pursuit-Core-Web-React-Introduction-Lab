b848096122c974b99b4f439c545d913dc27e9d8b
Jordan Bobadilla <jordanboba08@gmail.com>
2021-04-03 18:29:59 -0400

    completed lab

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import NavBar from "./Components/NavBar";
 import UserProfile from "./Components/UserProfile";
 import Posts from "./Components/Posts"
 import Contacts from "./Components/Contacts"
-// import './App.css'
+import './App.css'
  import "./index.css";
 
 export default function App() {
   return (
-    <>
+    <main className="Main-App">
       <NavBar />
       <UserProfile />
       <Posts/>
       <Contacts/>
-    </>
+    </main>
   );
 }
