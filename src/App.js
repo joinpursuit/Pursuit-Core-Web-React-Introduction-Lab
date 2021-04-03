4fea20d4480ff5c2af9c6672b9a1e7b23070799d
Coreen Cooper <coreencooper@pursuit.org>
2021-04-03 16:23:18 -0400

    create user object

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import NavBar from "./Components/NavBar.js";
 import UserProfile from "./Components/UserProfile.js";
-import Posts from "./Components/Posts.js"
-import Contacts from "./Components/Contacts.js"
+import Posts from "./Components/Posts.js";
+import Contacts from "./Components/Contacts.js";
 import "./index.css";
+
 
 export default function App() {
   return (
     <>
-  <UserProfile />
-  <Posts />
-  </>
-    )
+      <UserProfile />
+      <Posts />
+    </>
+  );
 }
-
--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,16 +1,26 @@
 import React from "react";
 
-function UserProfile () {
-    return (
-        <>
-        <img src="https://acaweb.org/wp-content/uploads/2018/12/profile-placeholder.png" alt="post" width="200" height="200"></img>
-        <h1>Jordan Walke</h1>
-        <p>React Creator</p>
-        <p>Lorem Ipsem</p>
-        </>
-    )
+const user = {
+    name: "Jordan Walke",
+    title: "React Creator",
+    subject: "Lorem Ipsem",
+  };
+  
+
+function UserProfile() {
+  return (
+    <>
+      <img
+        src="https://acaweb.org/wp-content/uploads/2018/12/profile-placeholder.png"
+        alt="post"
+        width="200"
+        height="200"
+      />
+      <h1>{user.name}</h1>
+      <p>{user.title}</p>
+      <p>{user.subject}</p>
+    </>
+  );
 }
-
-
 
 export default UserProfile;
