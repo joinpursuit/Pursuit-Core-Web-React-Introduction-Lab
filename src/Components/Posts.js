22b97b19c73632dbefac97e1cd2a3521299c3941
Flower Aviles <itsfloweraviles@gmail.com>
2021-04-03 16:20:50 -0400

    posts passed

---
+++ src/Components/Posts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Post  from "./Post";
+
+export function Posts() {
+  return (
+    <div id="posts">
+      <h2>Posts</h2>
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+}
