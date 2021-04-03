045fd953f4e8ecd55fd580b2429e021c80167f4a
joshuamendez <joshuafmendez@gmail.com>
2021-04-03 16:11:41 -0400

    file clean up

--- src/App.js
+++ src/App.js
@@ -6,19 +6,12 @@ import NavBar from './Components/NavBar.js'
 import "./index.css";
 
 export default function App() {
-  const naviBar = <NavBar />;
-  const h1 = <h1>Hello</h1>
-  const p = <p>World</p>;
-  // const h1p = h1 + p + naviBar;
-  // debugger
+
   return (
     <div className='App-Div'>
-    {console.log(naviBar)}
-    {console.log(h1)}
-    {/* {console.log(h1p)} */}
-    <NavBar />
-    <UserProfile />
-    <Posts />
-    <Contacts />
-  </div>);
+      <NavBar />
+      <UserProfile />
+      <Posts />
+      <Contacts />
+    </div>);
 }
