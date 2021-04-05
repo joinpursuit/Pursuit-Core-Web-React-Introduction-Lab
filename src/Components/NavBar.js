088835819baf6215db2f7fcaee040aaf5e81728d
cassidybeni <cassidybeni@pursuit.org>
2021-04-05 19:25:18 -0400

    Complete NavBar.js

---
+++ src/Components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const navBar = () => {
+    return (
+        <>
+           <button>What is Pursuit?</button>
+            <button>Create an account</button>
+            <button>Sign in</button>
+           
+        </>
+    )
+}
+
+export default navBar
