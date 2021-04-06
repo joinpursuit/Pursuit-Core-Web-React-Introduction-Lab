f0755619546f4882d34f16784b05daf1aa402fd0
cassidybeni <cassidybeni@pursuit.org>
2021-04-05 23:44:40 -0400

    Delete obj in user profile since component will not be duplicated

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,18 +1,12 @@
 import React from 'react'
 
-const userInfo = {
-    name: "Jordan Walke",
-    jobTitle: "React Creator",
-    description: "Lorem Ipsem",
-}
-
 const userProfile = () => {
     
     return (
         <div>
-            <h1>{userInfo.name}</h1>
-            <h2>{userInfo.jobTitle}</h2>
-            <p>{userInfo.description}</p>
+            <h1>Jordan Walke</h1>
+            <h2>React Creator</h2>
+            <p>Lorem Ipsem</p>
         </div>
     )
     
