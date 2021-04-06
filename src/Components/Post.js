af519ce6e812b7a91b28cade96a0a309004dda38
cassidybeni <cassidybeni@pursuit.org>
2021-04-05 21:50:00 -0400

    Complete Post.js

---
+++ src/Components/Post.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const post = () => {
+    return (
+        <>
+            <h1>Title</h1>
+            <p>Lorem Ipsem</p>
+        </>
+    )
+}
+
+export default post
---
+++ src/Components/Posts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const posts = () => {
+    return (
+        
+    )
+}
+
+export default posts
