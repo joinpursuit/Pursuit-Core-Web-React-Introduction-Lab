20e68cfb581c8750efd57176650b7de54a96e7be
Jenna Qian <jiayin1996@gmail.com>
2021-04-06 15:43:52 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
-
 import "./index.css";
 
+// import Contacts from "./Components/Contacts"
+// import ContactUserCard from "./Components/ContactUserCard"
+// import NavBar from "./Components/NavBar"
+// import Post from "./Components/Post"
+// import Posts from "./Components/Posts"
+// import UserProfile from "./Components/UserProfile"
+
+
+const helloMsg = "This is your first day on Earth."
+let name = "Jenna"
+
 const App = () => {
-  return <p>Hello, world!</p>;
+  return <p>Hello, {name}! {helloMsg}</p>;
 }
 
 export default App;
+
+
