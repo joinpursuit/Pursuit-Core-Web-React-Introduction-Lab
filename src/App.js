9a6397ddd1af52ced3ea6bc9a71d0382370beb54
Alexander <alexanderl96@yahoo.com>
2021-04-06 21:02:37 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import NavBar from "./Components/NavBar.js";
+import UserProfile from  "./Components/UserProfile"
+import Posts from "./Components/Posts"
+import Contacts from "./Components/Contacts"
+import "./index.css"
 
-import "./index.css";
 
-const App = () => {
-  return <p>Hello, world!</p>;
+function App(){
+  return(
+    <div className="App">
+      <NavBar />
+      <UserProfile />
+      <Posts />
+      <Contacts />
+    </div>
+  )
 }
-
-export default App;
+export default App
---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/navBar.css'
+
+const navegation = () =>{
+    return (
+        <nav>
+            <button>What is Pursuit?</button>
+            <button>Create an account</button>
+            <button>Sign in</button>
+        </nav>
+
+    )
+}
+export default navegation
