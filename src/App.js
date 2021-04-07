757166eded63ac9b20d69fc8e975ca0acdb913dd
Talia Pichardo <taliapichardo@gmail.com>
2021-04-07 14:38:51 -0400

    Posts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import NavBar from './Components/NavBar'
+import NavBar from './Components/NavBar';
+import Posts from './Components/Posts'
 import "./index.css";
 
 const App = () => {
-  return (<NavBar/>)
+  return (<NavBar/>, <Posts/>)
 }
 
 export default App;
