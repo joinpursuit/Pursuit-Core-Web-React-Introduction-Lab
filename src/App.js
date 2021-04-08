6c8d4bdd958cf5d4eb28b9bdefe487e40a710df0
Rayvon Finney Pinkston <rayfinn55@gmail.com>
2021-04-08 10:18:31 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
-
+import NavBar from './Components/NavBar.js'
+import UserProfile from './Components/UserProfile.js'
+import Posts from './Components/Posts.js'
+import Contacts from './Components/Contacts.js'
 import "./index.css";
 
 const App = () => {
-  return <p>Hello, world!</p>;
+  return <>
+   <NavBar/>
+   <UserProfile/>
+   <Posts/>
+   <Contacts/>
+   </>
 }
 
 export default App;
---
+++ src/Components/Contacts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ContactUserCard from './ContactUserCard.js'
+
+
+const Contacts = () => {
+    return <div>
+        <ContactUserCard/>
+        <ContactUserCard/>
+        <ContactUserCard/>
+        <ContactUserCard/>
+    </div>
+}
+
+export default Contacts
