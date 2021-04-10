6263d72d36b513fc64a86fd79d50287d608220f0
chidalgo1221 <carlos.hidalgo2017@mesacharter.org>
2021-04-10 10:12:46 -0400

    Finished

---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <>
+         <button type="button">What is Pursuit?</button>
+         <button type="button">Create an account</button>
+         <button type="button">Sign in</button>
+        </>
+
+    )
+}
+
+export default NavBar;
