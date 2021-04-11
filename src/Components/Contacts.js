adc4d93a059214ef78c8d49626a0855300905283
Du Min Chen <dm.chen0113@gmail.com>
2021-04-11 00:05:28 -0400

    React introduction lab complete

---
+++ src/Components/Contacts.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ContactUserCard from "./ContactUserCard"
+import "./Contacts.css"
+
+const Contacts = () => {
+    return (
+        <div className="contacts">
+            <h2>Contacts</h2>
+            <ContactUserCard />
+            <ContactUserCard />
+            <ContactUserCard />
+            <ContactUserCard />
+        </div>
+    )
+}
+
+export default Contacts
