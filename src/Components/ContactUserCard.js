a093d59fc332570fb505ba38b260b3e8b93f66f1
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-04-10 22:07:12 -0400

    complete lab

---
+++ src/Components/ContactUserCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ContactUserCard = () => {  
+return (
+  <div class>
+      <h2>Jordan Walke</h2>
+      <h3>React Creator</h3>
+      <p>Lorem Ipsem</p>
+    </div>
+)
+};
+
+export default ContactUserCard;
