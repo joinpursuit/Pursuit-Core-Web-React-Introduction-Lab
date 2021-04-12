1cefc4febb7758457e43f1d3818515860aecc3c0
Jasleen Villamil <jasleenvillamil@gmail.com>
2021-04-11 23:11:05 -0400

    Setting Up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 
 import "./index.css";
+import NavBar from "./Components/NavBar";
+import Posts from "./Components/Post";
+import UserProfile from "./Components/UserProfile";
+import Contact from "./Components/Contacts";
+
 
 const App = () => {
-  return <p>Hello, world!</p>;
+  return (
+    <div>
+      <NavBar/>
+      <Posts/>
+      <UserProfile/>
+      <Contact/>
+
+    </div>
+  );
 }
 
 export default App;
