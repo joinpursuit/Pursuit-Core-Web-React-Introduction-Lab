2bd6ff9d3c236f5d686d0319b25ffce684479b01
imran nazir <inazir2016@gmail.com>
2021-04-11 21:22:04 -0400

    initial work

---
+++ src/Components/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from './Post'
+function Posts(){
+    return(
+        <div>
+            <h3>Posts</h3>
+            <post />
+            <post />
+            <post />
+        </div>
+    )
+    
+}
+export default Posts;
